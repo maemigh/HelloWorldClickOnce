f457b42bb8aa408e2ab5c854a7048dead0770c8c
mike <maemigh@gmail.com>
2025-03-14 12:49:23 +0900

    Add project files.

---
+++ HelloWorldClickOnce/MainForm.cs
@@ -0,0 +1,22 @@
+﻿using System.Windows.Forms;
+
+namespace HelloWorldClickOnce
+{
+    public class MainForm : Form
+    {
+        public MainForm()
+        {
+            this.Text = "Hello World ClickOnce App";
+            this.StartPosition = FormStartPosition.CenterScreen;
+            this.Width = 300;
+            this.Height = 200;
+
+            Label helloLabel = new Label();
+            helloLabel.Text = "Hello, World!";
+            helloLabel.AutoSize = true;
+            helloLabel.Location = new System.Drawing.Point(100, 80);
+
+            this.Controls.Add(helloLabel);
+        }
+    }
+}
---
+++ HelloWorldClickOnce/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace HelloWorldClickOnce
+{
+    static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new MainForm());
+        }
+    }
+
+    public class MainForm : Form
+    {
+        public MainForm()
+        {
+            this.Text = "Hello World ClickOnce App";
+            this.StartPosition = FormStartPosition.CenterScreen;
+            this.Width = 300;
+            this.Height = 200;
+
+            Label helloLabel = new Label();
+            helloLabel.Text = "Hello, World!";
+            helloLabel.AutoSize = true;
+            helloLabel.Location = new System.Drawing.Point(100, 80);
+
+            this.Controls.Add(helloLabel);
+        }
+    }
+}
